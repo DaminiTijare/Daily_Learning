4948f86a3f40e09d46dca02a76d14b07b14739d8
DaminiTijare <82694779+DaminiTijare@users.noreply.github.com>
2023-04-27 17:07:17 +0530

    removing the comment

--- Codingexercise16.py
+++ Codingexercise16.py
@@ -1,18 +1,18 @@
-# import PySimpleGUI as st
+import PySimpleGUI as st
 
-# label=st.Text("Enter feet")
-# add_input=st.InputText()
+label=st.Text("Enter feet")
+add_input=st.InputText()
 
-# label1=st.Text("Enter inches")
-# add_input1=st.InputText()
+label1=st.Text("Enter inches")
+add_input1=st.InputText()
 
-# convert=st.Button('Convert')
+convert=st.Button('Convert')
 
-# window=st.Window('Convertor',layout=[[label,add_input],
-#                                      [label1,add_input1]
-#                                      ,[convert]])
-# window.read()
-# window.close()
+window=st.Window('Convertor',layout=[[label,add_input],
+                                     [label1,add_input1]
+                                     ,[convert]])
+window.read()
+window.close()
 
 
 import PySimpleGUI as sg
